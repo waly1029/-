e089de9558a53c589643d9df7471e804d4e89e4a
lz <waly1019@gmail.com>
2023-04-04 11:06:28 +0800

    update 404

--- LeetCode/404.左叶子之和.cpp
+++ LeetCode/404.左叶子之和.cpp
@@ -14,16 +14,22 @@ public:
     int sumOfLeftLeaves(TreeNode* root) {
         return process(root);
     }
-
+    // 后序遍历
     int process(TreeNode* node) {
+        // 根节点为空返回0
         if(!node) return 0;
+
+        // 叶子节点返回0,此时无法判断是否是左叶子
         if(!node->left && !node->right) return 0;
+
+        // 后序遍历左节点,判断是否有做孩子，并且左孩子为叶子节点
         int leftNum = process(node->left);
-        if(node->left!=nullptr && node->left->left==nullptr
-                               && node->left->right==nullptr)
-            leftNum = node->left->val;
+        if(node->left && !node->left->left && !node->left->right)
+            leftNum += node->left->val;
+
+        // 后序遍历右节点
         int rightNum = process(node->right);
-        int sum = leftNum + rightNum;
-        return sum;
+
+        return leftNum + rightNum;
     }
 };
